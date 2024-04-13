722f7cdb87e1212dfc5481d75069c34d22a8cdc8
Avi Kenny <avi.kenny@gmail.com>
2024-04-13 09:39:31 -0400

    Update config.R

--- R/config.R
+++ R/config.R
@@ -1,5 +1,5 @@
 # Change library path
-.libPaths(c("/home/akenny/R_lib", .libPaths()))
+.libPaths(c("/home/akenny/R_lib", "/hpc/home/ak811/R_lib", .libPaths()))
 
 # Set packages
 # GitHub packages: tedwestling/ctsCausal, tedwestling/CFsurvival,
@@ -22,11 +22,11 @@ if (Sys.getenv("HOME")=="/home/akenny") {
     dir = paste0("/home/akenny/", Sys.getenv("proj"),
                  "/Code__", Sys.getenv("proj"))
   )
-} else if (Sys.getenv("HOME")=="/home/users/avikenny") {
-  # Bayes
+} else if (Sys.getenv("HOME")=="/hpc/home/ak811") {
+  # DCC
   cluster_config <- list(
-    js = "ge",
-    dir = paste0("/home/users/avikenny/Desktop/", Sys.getenv("proj"),
+    js = "slurm",
+    dir = paste0("/hpc/home/ak811/", Sys.getenv("proj"),
                  "/Code__", Sys.getenv("proj"))
   )
 } else {
@@ -34,7 +34,7 @@ if (Sys.getenv("HOME")=="/home/akenny") {
 }
 
 # Load packages (if running locally or not running sims)
-if (Sys.getenv("USERDOMAIN")=="AVI-KENNY-T460" || !cfg$run_sims) {
+if (Sys.getenv("RSTUDIO")=="1" || !cfg$run_sims) {
   for (pkg in c(cfg$pkgs,cfg$pkgs_nocluster)) {
     suppressMessages({ do.call("library", list(pkg)) })
   }
